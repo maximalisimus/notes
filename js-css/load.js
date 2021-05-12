39d5635802466942c343b8ed7e4652022ea18c26
Mikhail Artamonov <maximalis171091@yandex.ru>
2021-05-12 19:39:48 +0300

    Update

--- js-css/load.js
+++ js-css/load.js
@@ -1,21 +1,34 @@
-document.querySelector('.container').onclick = event => {
-    if (event.target.tagName === 'A' && event.target.innerHTML == 'Open') {
-        const frame = event.target.nextElementSibling;
-        frame.style.display='block';
-        event.target.innerHTML='Close';
-    } else if (event.target.tagName === 'A' && event.target.innerHTML == 'Close') {
-        const frame = event.target.nextElementSibling;
-        frame.style.display='none';
-        event.target.innerHTML='Open';
-    } else if (event.target.tagName === 'A' && event.target.className == 'nexter') {
-		const blockdiv = event.target.nextElementSibling;
-		if (blockdiv.style.display == "none") {
-			blockdiv.style.display='block';
-			event.target.innerHTML='Свернуть ...';
-		} else {
-			blockdiv.style.display='none';
-			event.target.innerHTML='Развернуть ...';
+var elements = document.querySelectorAll('.container');
+
+for (let elem of elements) {
+	elem.onclick = event => {
+		if (event.target.tagName === 'A' && event.target.className == 'nexter') {
+			const blockdiv = event.target.nextElementSibling;
+			if (blockdiv.style.display == "none") {
+				blockdiv.style.display='block';
+				event.target.innerHTML='Свернуть&nbsp;...';
+				blockdiv.setAttribute("id","actives");
+			} else {
+				blockdiv.style.display='none';
+				event.target.innerHTML='Развернуть&nbsp;...';
+				blockdiv.removeAttribute("id");
+			}
+			return false;
+		} else if (event.target.tagName === 'A' && event.target.className == 'read') {
+			const frame = document.getElementById("frames");
+			if (frame.style.display == "none") {
+				frame.style.display='block';
+				frame.scrollIntoView({block: "start", inline: "center", behavior: "smooth"});
+			} else {
+				frame.style.display='none';
+			}
 		}
-		return false;
 	}
+}
+
+function CloseFrame() {
+	const frame = document.getElementById("frames");
+	frame.style.display='none';
+	const activeblock = document.getElementById('actives');
+	activeblock.scrollIntoView({block: "start", inline: "center", behavior: "smooth"});
 }
