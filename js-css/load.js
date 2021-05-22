dd1b1728b59f9485102ece8d9490519877304250
Mikhail Artamonov <maximalis171091@yandex.ru>
2021-05-23 02:05:09 +0300

    Update

--- js-css/load.js
+++ js-css/load.js
@@ -3,34 +3,42 @@ var elements = document.querySelectorAll('.container');
 for (let elem of elements) {
 	elem.onclick = event => {
 		if (event.target.tagName === 'A' && event.target.className == 'nexter') {
-			const blockdiv = event.target.nextElementSibling;
-			if (blockdiv.style.display == "none") {
-				blockdiv.style.display='block';
+			const blockmore = event.target.nextElementSibling;
+			if (blockmore.style.display == "none") {
+				blockmore.style.display='block';
 				event.target.innerHTML='Свернуть&nbsp;...';
-				event.target.setAttribute("id","actives");
 			} else {
-				blockdiv.style.display='none';
+				blockmore.style.display='none';
 				event.target.innerHTML='Развернуть&nbsp;...';
-				event.target.removeAttribute("id");
 			}
 			return false;
 		} else if (event.target.tagName === 'A' && event.target.className == 'read') {
-			const frame = document.getElementById("frames");
+			const frame = document.getElementById(event.target.target);
 			if (frame.style.display == "none") {
+				const frameid = event.target.target + "_rd"
+				event.target.setAttribute("id",frameid);
 				frame.style.display='block';
 				frame.scrollIntoView({block: "start", inline: "center", behavior: "smooth"});
+				event.target.innerHTML='Закрыть чтение&nbsp;...';
 			} else {
 				frame.style.display='none';
+				event.target.removeAttribute("id");
+				event.target.innerHTML='Читать онлайн&nbsp;...';
 			}
+			
 		}
 	}
 }
 
-function CloseFrame() {
-	const frame = document.getElementById("frames");
-	frame.style.display='none';
-	if (document.getElementById('actives')) {
-		const activeblock = document.getElementById('actives').previousElementSibling;
-		activeblock.scrollIntoView({block: "start", inline: "center", behavior: "smooth"});
+function CloseFrame(frameName) {
+	const blockframe = document.getElementById(frameName);
+	blockframe.style.display='none';
+	const blockid = frameName + "_rd"
+	if (document.getElementById(blockid)) {
+		const positionid = document.getElementById(blockid);
+		const position = positionid.parentElement;
+		position.scrollIntoView({block: "start", inline: "center", behavior: "smooth"});
+		positionid.removeAttribute("id");
+		positionid.innerHTML='Читать онлайн&nbsp;...';
 	}
 }
