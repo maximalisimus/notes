ab78957b8a7db6d5b2e274d78b78a75470f35d03
Mikhail Artamonov <maximalis171091@yandex.ru>
2022-01-06 04:09:16 +0300

    Update

--- js-css/more.js
+++ js-css/more.js
@@ -1,5 +1,15 @@
 var elements = document.querySelectorAll('.content');
 
+function copyToClipboard(str) {
+ var area = document.createElement('textarea');
+ 
+ document.body.appendChild(area);
+ area.value = str;
+ area.select();
+ document.execCommand("copy");
+ document.body.removeChild(area);
+}
+
 for (let elem of elements) {
 	elem.onclick = event => {
 		if (event.target.tagName === 'A' && event.target.className == 'nextmore') {
@@ -12,6 +22,22 @@ for (let elem of elements) {
 				event.target.innerHTML='Анотация&nbsp;...';
 			}
 			return false;
+		} else if (event.target.tagName === 'A' && event.target.className == 'category') {
+			const moreblock = event.target.nextElementSibling;
+			if (moreblock.style.display == "none") {
+				moreblock.style.display='block';
+				event.target.innerHTML='Свернуть&nbsp;...';
+			} else {
+				moreblock.style.display='none';
+				event.target.innerHTML='Развернуть&nbsp;...';
+			}
+			return false;
+		} else if (event.target.tagName === 'LI') {
+			let selli = document.querySelector('li:hover');
+			selli.addEventListener('click', ()=>{
+				copyToClipboard(selli.innerHTML)
+			})
 		}
 	}
 }
+
